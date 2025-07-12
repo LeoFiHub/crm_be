1ec94b0a8af420e3e80b91a476d15532ef5db2c6
Truong Nguyen <vonguyen.0407@gmail.com>
2025-07-12 14:46:39 +0700

    update - add password for user model

--- models/user.model.js
+++ models/user.model.js
@@ -5,6 +5,7 @@ const sequelize = require('../config/sequelize.config');
 const User = sequelize.define('User', {
   fullName: { type: DataTypes.STRING, allowNull: false },
   email: { type: DataTypes.STRING },
+    password: { type: DataTypes.STRING, allowNull: false }, // Thêm field password
   phoneNumber: { type: DataTypes.STRING },
   walletAddress: { type: DataTypes.STRING },
   salary: { type: DataTypes.FLOAT }, // chỉ dùng cho employee
