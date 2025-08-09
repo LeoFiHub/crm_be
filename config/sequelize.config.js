8c0ccfcce34beb380306bd13e45061405e20dd43
Truong Nguyen <vonguyen.0407@gmail.com>
2025-08-09 21:18:02 +0700

    done: connect to online database

--- config/sequelize.config.js
+++ config/sequelize.config.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'crm_blockchain',
-  process.env.DB_USER || 'root', 
-  process.env.DB_PASSWORD || '',
+  process.env.DB_NAME ,
+  process.env.DB_USER , 
+  process.env.DB_PASSWORD ,
   {
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
+    host: process.env.DB_HOST ,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     logging: false,
     pool: {
@@ -15,6 +15,12 @@ const sequelize = new Sequelize(
       min: 0,
       acquire: 30000,
       idle: 10000
+    },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // cho phép cert tự ký
+      }
     }
   }
 );
